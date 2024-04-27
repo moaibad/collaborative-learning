bb8d354724760b04f2741ed0147735f8b3a82c05
FadhilRadja <Assydiq124@gmail.com>
2024-04-27 12:11:35 +0700

    Update Dashboard Dosen (QuizList)

--- frontend/src/components/dashboardDosen/DashboardDosenQuizList.jsx
+++ frontend/src/components/dashboardDosen/DashboardDosenQuizList.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import { Progress } from 'antd';
+import React, { useState, useEffect } from 'react'
 import { TbNotesOff } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import { HOST_MOODLE, TOKEN_MOODLE } from "../../lib/env";
+import Cookies from 'js-cookie';
 import courses from '../../data/dataCourse';
 
 const DashboardQuizList = () => {
+    const [quizzes, setQuizzes] = useState([]);
+    const userIdMoodle = Cookies.get('userIdMoodle');
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          let api;
+          api = `${HOST_MOODLE}/webservice/rest/server.php?wstoken=${TOKEN_MOODLE}&moodlewsrestformat=json&wsfunction=local_colle_get_quiz&userid=${userIdMoodle}`;
+          const response = await fetch(api);
+          const data = await response.json();
+          setQuizzes(data);
+        } catch (error) {
+          console.error("Error fetching quiz data:", error);
+        }
+      };
+
+      fetchData();
+    });
+
     // Check if courses array is empty
-    if (!Array.isArray(courses) || courses.length === 0) {
+    if (!Array.isArray(quizzes) || quizzes.length === 0) {
         return (
           <div className="w-full">
             <p className='text-xl font-bold mb-6'>Course List</p>
@@ -15,40 +36,38 @@ const DashboardQuizList = () => {
                     <div className='place-self-center'>
                       <TbNotesOff size={72} />
                     </div>
-                    <p className='font-semibold'>Tidak ada course yang diikuti</p>
+                    <p className='font-semibold'>Tidak ada Quiz yang dibuat</p>
                 </div>
             </div>
           </div>
         );
       }
 
-    const coursesToShow = courses.slice(0, 3);
+    const quizToShow = quizzes.slice(0, 3);
 
     return (
       <div className="w-full">
         <div className='flex justify-between'>
           <p className='text-xl font-bold mb-6'>Your Quiz List</p>
-          {coursesToShow.length > 2 &&
-            <p className='text-orange-400 font-bold'>Lihat Selengkapnya</p>
+          {quizToShow.length > 2 &&
+            <Link to="/quiz" className='text-orange-400 font-bold'>Lihat Selengkapnya</Link>
           }
         </div>
         <div className='flex gap-4'>
-          {coursesToShow.map((course, index) => (
-            <div key={index} className='rounded-lg bg-white shadow-md h-82 w-72'>
-              <img className='m-0 p-0 w-72 h-36 object-cover rounded-t-lg' src={course.imageCourse} alt="" />
-              <div className='p-3'>
-                <div className='bottom-2 leading-6'>
-                  <div className='flex gap-2'>
-                    <img className='h-6 w-6 rounded-full' src={course.imageLecturer} alt="" />
-                    <p>{course.instructor}</p>
-                  </div>
-                  <p className='font-bold text-xl'>{course.title}</p>
-                  {/* <div className='mt-2 items-center'>
-                    <Progress percent={course.progress} status="active" />
-                  </div> */}
+          {quizToShow.map((quiz) => (
+            <Link key={quiz.id} to={quiz.url}>
+              <div className='rounded-lg bg-white shadow-md h-82 w-72'>
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{quiz.name}</div>
+                  <hr className="border-1 border-black" />
+                  <p className="text-gray-700 text-base">{quiz.intro}</p>
+                  <br></br>
+                  <p className="text-gray-700 text-base font-bold italic">
+                    Total Questions : <span className="font-bold italic">{quiz.total_questions}</span>
+                  </p>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
