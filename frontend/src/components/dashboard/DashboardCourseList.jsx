b26e43cc7b0a94f624c5936f935c8f82a42e94eb
Al Ghani <alghani63@gmail.com>
2024-04-27 17:12:01 +0700

    update minor DashboardCourseList.jsx

--- frontend/src/components/dashboard/DashboardCourseList.jsx
+++ frontend/src/components/dashboard/DashboardCourseList.jsx
@@ -22,7 +22,7 @@ const DashboardCourseList = () => {
 
     const fetchData = async () => {
       
-      if(userIdMoodle == null){
+      if(userIdMoodle != null){
         userIdMoodle = '4'
       }
 
@@ -55,7 +55,7 @@ const DashboardCourseList = () => {
     },[]); 
 
       // Menampilkan pesan jika tidak ada kursus atau kurang dari 3 kursus
-      if (!Array.isArray(courses) || courses.length === 1) {
+      if (!Array.isArray(courses) || courses.length === 0) {
         return (
             <div className="w-full">
                 <p className='text-xl font-bold mb-6'>Course List</p>
@@ -71,47 +71,60 @@ const DashboardCourseList = () => {
         );
     }
 
+    if (courses.length === 1 && courses[0].displayname === "Quiz") {
+        return (
+            <div className="w-full">
+                <p className='text-xl font-bold mb-6'>Course List</p>
+                <div className='w-full rounded-lg bg-white text-center h-82 border-4 border-dashed'>
+                    <div className='text-slate-400 text-center p-8 gap-4 grid grid-cols-1'>
+                        <div className='place-self-center'>
+                            <TbNotesOff size={72} />
+                        </div>
+                        <p className='font-semibold'>Tidak ada kursus yang diikuti</p>
+                    </div>
+                </div>
+            </div>
+        );
+      }
+
     // Menampilkan maksimal 3 kursus
-    const coursesToShow = courses.slice(1, 4);
+    const coursesToShow = courses.slice(0, 3);
 
     return (
-      <div className="w-full">
+        <div className="w-full">
           <div className='flex justify-between'>
-              <p className='text-xl font-bold mb-6'>Course List</p>
-              {coursesToShow.length > 2 &&
-                  <p className='text-orange-400 font-bold' onClick={handleClick}>Lihat Selengkapnya</p>
-              }
+            <p className='text-xl font-bold mb-6'>Course List</p>
+            {coursesToShow.length > 3 &&
+              <p className='text-orange-400 font-bold' onClick={handleClick}>Lihat Selengkapnya</p>
+            }
           </div>
           <div className='flex gap-4'>
-              {coursesToShow.map((course, index) => (
-                  <div key={index} className='rounded-lg bg-white shadow-md h-82 w-72'>
-                      <Link to={`${HOST_MOODLE}/course/view.php?id=${course.id}`}>
-                          <img className='m-0 p-0 w-72 h-36 object-cover rounded-t-lg' src={coursePage2} alt="" />
-                          {/* <img className='m-0 p-0 w-72 h-36 object-cover rounded-t-lg' src={course.courseimage} alt="" /> */}
-                          <div className='p-3'>
-                              <div className='bottom-2 leading-6'>
-                                  {/* <div className='flex gap-2'>
-                                      <img className='h-6 w-6 rounded-full' src={course.imageLecturer} alt="" />
-                                      <img className='h-6 w-6 rounded-full' src={dummyLecturePic} alt="" />
-                                      <p>{course.instructor}</p>
-                                      <p>Dummy Nama Dosen</p>
-                                  </div> */}
-                                  {/* Menggunakan Link untuk mengelilingi judul kursus */}
-                                  <Link to={`${HOST_MOODLE}/course/view.php?id=${course.id}`}>
-                                      <p className='font-bold text-xl'>{course.displayname}</p>
-                                  </Link>
-                                  {/* <p className='font-bold text-xl'>awe</p> */}
-                                  {/* <div className='mt-2 items-center'>
-                                      <Progress percent={course.progress} status="active" />
-                                  </div> */}
-                              </div>
-                          </div>
-                      </Link>
-                  </div>
-              ))}
+            {coursesToShow.map((course, index) => (
+              // Check if displayname is not 'Quiz'
+              course.displayname !== 'Quiz' && (
+                <div key={index} className='rounded-lg bg-white shadow-md h-82 w-72'>
+                  <Link to={`${HOST_MOODLE}/course/view.php?id=${course.id}`}>
+                    <img className='m-0 p-0 w-72 h-36 object-cover rounded-t-lg' src={coursePage2} alt="" />
+                    {/* <img className='m-0 p-0 w-72 h-36 object-cover rounded-t-lg' src={course.courseimage} alt="" /> */}
+                    <div className='p-3'>
+                      <div className='bottom-2 leading-6'>
+                        {/* Menggunakan Link untuk mengelilingi judul kursus */}
+                        <Link to={`${HOST_MOODLE}/course/view.php?id=${course.id}`}>
+                          <p className='font-bold text-xl'>{course.displayname}</p>
+                        </Link>
+                        {/* <p className='font-bold text-xl'>awe</p> */}
+                        {/* <div className='mt-2 items-center'>
+                            <Progress percent={course.progress} status="active" />
+                        </div> */}
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              )
+            ))}
           </div>
-      </div>
-  );
+        </div>
+      );      
 };
 
 export default DashboardCourseList
